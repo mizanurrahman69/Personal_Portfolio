fbfc565b1979d6857076de47afe01099e2c39382
mizanurrahman69 <abdulhamidemon0@gmail.com>
2024-03-04 23:13:49 +0600

    commit latest design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,5 @@
 window.Alpine = Alpine
+
+var mapURL = "https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY&q=Space+Needle,Seattle+WA";
+
+document.getElementById("map").innerHTML = '<iframe width="100%" height="100%" frameborder="0" style="border:0" src="' + mapURL + '" allowfullscreen></iframe>';
